f4e22eda909ab7920403eb7c62225bdd34f17997
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-10-18 20:57:19 +0100

    Modifying IDs for easier game logic

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,60 +3,6 @@ db.dropDatabase();
 
 db.Cards.insertMany([
   {
-    id: 1,
-    colour: "blue",
-    number: 1,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
-    id: 2,
-    colour: "blue",
-    number: 2,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
-    id: 3,
-    colour: "blue",
-    number: 3,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
-    id: 4,
-    colour: "blue",
-    number: 4,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
-    id: 5,
-    colour: "blue",
-    number: 5,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
-    id: 6,
-    colour: "blue",
-    number: 6,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
-    id: 7,
-    colour: "blue",
-    number: 7,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
-    id: 8,
-    colour: "blue",
-    number: 8,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
-    id: 9,
-    colour: "blue",
-    number: 9,
-    url: 'http://localhost:3000/images/green_1.png'
-  },
-  {
     id: 10,
     colour: "blue",
     number: 10,
@@ -64,55 +10,55 @@ db.Cards.insertMany([
   },
   {
     id: 11,
-    colour: "red",
+    colour: "blue",
     number: 1,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 12,
-    colour: "red",
+    colour: "blue",
     number: 2,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 13,
-    colour: "red",
+    colour: "blue",
     number: 3,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 14,
-    colour: "red",
+    colour: "blue",
     number: 4,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 15,
-    colour: "red",
+    colour: "blue",
     number: 5,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 16,
-    colour: "red",
+    colour: "blue",
     number: 6,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 17,
-    colour: "red",
+    colour: "blue",
     number: 7,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 18,
-    colour: "red",
+    colour: "blue",
     number: 8,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 19,
-    colour: "red",
+    colour: "blue",
     number: 9,
     url: 'http://localhost:3000/images/green_1.png'
   },
@@ -124,55 +70,55 @@ db.Cards.insertMany([
   },
   {
     id: 21,
-    colour: "green",
+    colour: "red",
     number: 1,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 22,
-    colour: "green",
+    colour: "red",
     number: 2,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 23,
-    colour: "green",
+    colour: "red",
     number: 3,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 24,
-    colour: "green",
+    colour: "red",
     number: 4,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 25,
-    colour: "green",
+    colour: "red",
     number: 5,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 26,
-    colour: "green",
+    colour: "red",
     number: 6,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 27,
-    colour: "green",
+    colour: "red",
     number: 7,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 28,
-    colour: "green",
+    colour: "red",
     number: 8,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 29,
-    colour: "green",
+    colour: "red",
     number: 9,
     url: 'http://localhost:3000/images/green_1.png'
   },
@@ -184,55 +130,55 @@ db.Cards.insertMany([
   },
   {
     id: 31,
-    colour: "yellow",
+    colour: "green",
     number: 1,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 32,
-    colour: "yellow",
+    colour: "green",
     number: 2,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 33,
-    colour: "yellow",
+    colour: "green",
     number: 3,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 34,
-    colour: "yellow",
+    colour: "green",
     number: 4,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 35,
-    colour: "yellow",
+    colour: "green",
     number: 5,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 36,
-    colour: "yellow",
+    colour: "green",
     number: 6,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 37,
-    colour: "yellow",
+    colour: "green",
     number: 7,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 38,
-    colour: "yellow",
+    colour: "green",
     number: 8,
     url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 39,
-    colour: "yellow",
+    colour: "green",
     number: 9,
     url: 'http://localhost:3000/images/green_1.png'
   },
@@ -240,6 +186,60 @@ db.Cards.insertMany([
     id: 40,
     colour: "yellow",
     number: 10,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 41,
+    colour: "yellow",
+    number: 1,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 42,
+    colour: "yellow",
+    number: 2,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 43,
+    colour: "yellow",
+    number: 3,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 44,
+    colour: "yellow",
+    number: 4,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 45,
+    colour: "yellow",
+    number: 5,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 46,
+    colour: "yellow",
+    number: 6,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 47,
+    colour: "yellow",
+    number: 7,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 48,
+    colour: "yellow",
+    number: 8,
+    url: 'http://localhost:3000/images/green_1.png'
+  },
+  {
+    id: 49,
+    colour: "yellow",
+    number: 9,
     url: 'http://localhost:3000/images/green_1.png'
   }
 ]);
