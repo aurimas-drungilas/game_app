12b4da2325ef91051b96958f2e7588a9ecbe8b27
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-10-18 21:01:23 +0100

    Adding cards-misc route to the server

--- server/server.js
+++ server/server.js
@@ -18,9 +18,13 @@ MongoClient.connect(
 )
 .then((client) => {
   const db = client.db('game');
-  const unoCollection = db.collection('Cards');
-  const unoRouter = createRouter(unoCollection);
-  app.use('/api/cards', unoRouter);
+  const cardsCollection = db.collection('Cards');
+  const cardsRouter = createRouter(cardsCollection);
+  app.use('/api/cards', cardsRouter);
+
+  const cardsMiscCollection = db.collection('CardsMisc');
+  const cardsMiscRouter = createRouter(cardsMiscCollection);
+  app.use('/api/cards-misc', cardsMiscRouter);
 })
 .catch(console.err);
 
