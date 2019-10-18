d9832fc52091e4ab38312df2711a33f7f4e58e83
AdamHidvegi <hidvegiadam@gmail.com>
2019-10-18 18:40:55 +0100

    database without urls

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,17 +1,245 @@
 use game;
 db.dropDatabase();
 
-db.uno.insertMany([
+db.Cards.insertMany([
   {
-    name: "Test",
-    bool: true
+    id: 1,
+    colour: "blue",
+    number: 1,
+    url: ''
   },
   {
-    name: "Test 2",
-    bool: false
+    id: 2,
+    colour: "blue",
+    number: 2,
+    url: ''
   },
   {
-    name: "Test 3",
-    bool: true
+    id: 3,
+    colour: "blue",
+    number: 3,
+    url: ''
+  },
+  {
+    id: 4,
+    colour: "blue",
+    number: 4,
+    url: ''
+  },
+  {
+    id: 5,
+    colour: "blue",
+    number: 5,
+    url: ''
+  },
+  {
+    id: 6,
+    colour: "blue",
+    number: 6,
+    url: ''
+  },
+  {
+    id: 7,
+    colour: "blue",
+    number: 7,
+    url: ''
+  },
+  {
+    id: 8,
+    colour: "blue",
+    number: 8,
+    url: ''
+  },
+  {
+    id: 9,
+    colour: "blue",
+    number: 9,
+    url: ''
+  },
+  {
+    id: 10,
+    colour: "blue",
+    number: 10,
+    url: ''
+  },
+  {
+    id: 11,
+    colour: "red",
+    number: 1,
+    url: ''
+  },
+  {
+    id: 12,
+    colour: "red",
+    number: 2,
+    url: ''
+  },
+  {
+    id: 13,
+    colour: "red",
+    number: 3,
+    url: ''
+  },
+  {
+    id: 14,
+    colour: "red",
+    number: 4,
+    url: ''
+  },
+  {
+    id: 15,
+    colour: "red",
+    number: 5,
+    url: ''
+  },
+  {
+    id: 16,
+    colour: "red",
+    number: 6,
+    url: ''
+  },
+  {
+    id: 17,
+    colour: "red",
+    number: 7,
+    url: ''
+  },
+  {
+    id: 18,
+    colour: "red",
+    number: 8,
+    url: ''
+  },
+  {
+    id: 19,
+    colour: "red",
+    number: 9,
+    url: ''
+  },
+  {
+    id: 20,
+    colour: "red",
+    number: 10,
+    url: ''
+  },
+  {
+    id: 21,
+    colour: "green",
+    number: 1,
+    url: ''
+  },
+  {
+    id: 22,
+    colour: "green",
+    number: 2,
+    url: ''
+  },
+  {
+    id: 23,
+    colour: "green",
+    number: 3,
+    url: ''
+  },
+  {
+    id: 24,
+    colour: "green",
+    number: 4,
+    url: ''
+  },
+  {
+    id: 25,
+    colour: "green",
+    number: 5,
+    url: ''
+  },
+  {
+    id: 26,
+    colour: "green",
+    number: 6,
+    url: ''
+  },
+  {
+    id: 27,
+    colour: "green",
+    number: 7,
+    url: ''
+  },
+  {
+    id: 28,
+    colour: "green",
+    number: 8,
+    url: ''
+  },
+  {
+    id: 29,
+    colour: "green",
+    number: 9,
+    url: ''
+  },
+  {
+    id: 30,
+    colour: "green",
+    number: 10,
+    url: ''
+  },
+  {
+    id: 31,
+    colour: "yellow",
+    number: 1,
+    url: ''
+  },
+  {
+    id: 32,
+    colour: "yellow",
+    number: 2,
+    url: ''
+  },
+  {
+    id: 33,
+    colour: "yellow",
+    number: 3,
+    url: ''
+  },
+  {
+    id: 34,
+    colour: "yellow",
+    number: 4,
+    url: ''
+  },
+  {
+    id: 35,
+    colour: "yellow",
+    number: 5,
+    url: ''
+  },
+  {
+    id: 36,
+    colour: "yellow",
+    number: 6,
+    url: ''
+  },
+  {
+    id: 37,
+    colour: "yellow",
+    number: 7,
+    url: ''
+  },
+  {
+    id: 38,
+    colour: "yellow",
+    number: 8,
+    url: ''
+  },
+  {
+    id: 39,
+    colour: "yellow",
+    number: 9,
+    url: ''
+  },
+  {
+    id: 40,
+    colour: "yellow",
+    number: 10,
+    url: ''
   }
 ]);
