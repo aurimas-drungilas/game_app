235093defb6e84d2a103199ec92b4d7abe146e41
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-10-18 20:25:42 +0100

    Adding the sample links to db seed

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -6,240 +6,240 @@ db.Cards.insertMany([
     id: 1,
     colour: "blue",
     number: 1,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 2,
     colour: "blue",
     number: 2,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 3,
     colour: "blue",
     number: 3,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 4,
     colour: "blue",
     number: 4,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 5,
     colour: "blue",
     number: 5,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 6,
     colour: "blue",
     number: 6,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 7,
     colour: "blue",
     number: 7,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 8,
     colour: "blue",
     number: 8,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 9,
     colour: "blue",
     number: 9,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 10,
     colour: "blue",
     number: 10,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 11,
     colour: "red",
     number: 1,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 12,
     colour: "red",
     number: 2,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 13,
     colour: "red",
     number: 3,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 14,
     colour: "red",
     number: 4,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 15,
     colour: "red",
     number: 5,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 16,
     colour: "red",
     number: 6,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 17,
     colour: "red",
     number: 7,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 18,
     colour: "red",
     number: 8,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 19,
     colour: "red",
     number: 9,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 20,
     colour: "red",
     number: 10,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 21,
     colour: "green",
     number: 1,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 22,
     colour: "green",
     number: 2,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 23,
     colour: "green",
     number: 3,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 24,
     colour: "green",
     number: 4,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 25,
     colour: "green",
     number: 5,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 26,
     colour: "green",
     number: 6,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 27,
     colour: "green",
     number: 7,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 28,
     colour: "green",
     number: 8,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 29,
     colour: "green",
     number: 9,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 30,
     colour: "green",
     number: 10,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 31,
     colour: "yellow",
     number: 1,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 32,
     colour: "yellow",
     number: 2,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 33,
     colour: "yellow",
     number: 3,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 34,
     colour: "yellow",
     number: 4,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 35,
     colour: "yellow",
     number: 5,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 36,
     colour: "yellow",
     number: 6,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 37,
     colour: "yellow",
     number: 7,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 38,
     colour: "yellow",
     number: 8,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 39,
     colour: "yellow",
     number: 9,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   },
   {
     id: 40,
     colour: "yellow",
     number: 10,
-    url: ''
+    url: 'http://localhost:3000/images/green_1.png'
   }
 ]);
