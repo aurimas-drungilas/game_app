10f1389bd2b3c97c00eef28d8761b465e1ba17b1
AdamHidvegi <hidvegiadam@gmail.com>
2019-10-21 22:21:06 +0100

    almost ready to go

--- client/src/components/play/PlayerCard.vue
+++ client/src/components/play/PlayerCard.vue
@@ -14,62 +14,7 @@ export default {
     handleClick() {
       eventBus.$emit('player-card-clicked', this.card);
     }
-  },
-
-
-  // // enable draggables to be dropped into this
-  // interact('.player-card').dropzone({
-  //   // only accept elements matching this CSS selector
-  //   accept: '#yes-drop',
-  //   // Require a 75% element overlap for a drop to be possible
-  //   overlap: 0.75,
-  //
-  //   // listen for drop related events:
-  //
-  //   ondropactivate: function (event) {
-  //     // add active dropzone feedback
-  //     event.target.classList.add('drop-active')
-  //   },
-  //   ondragenter: function (event) {
-  //     var draggableElement = event.relatedTarget
-  //     var dropzoneElement = event.target
-  //
-  //     // feedback the possibility of a drop
-  //     dropzoneElement.classList.add('drop-target')
-  //     draggableElement.classList.add('can-drop')
-  //     draggableElement.textContent = 'Dragged in'
-  //   },
-  //   ondragleave: function (event) {
-  //     // remove the drop feedback style
-  //     event.target.classList.remove('drop-target')
-  //     event.relatedTarget.classList.remove('can-drop')
-  //     event.relatedTarget.textContent = 'Dragged out'
-  //   },
-  //   ondrop: function (event) {
-  //     event.relatedTarget.textContent = 'Dropped'
-  //   },
-  //   ondropdeactivate: function (event) {
-  //     // remove active dropzone feedback
-  //     event.target.classList.remove('drop-active')
-  //     event.target.classList.remove('drop-target')
-  //   }
-  // })
-  //
-  // interact('.drag-drop')
-  // .draggable({
-  //   inertia: true,
-  //   modifiers: [
-  //     interact.modifiers.restrictRect({
-  //       restriction: 'parent',
-  //       endOnly: true
-  //     })
-  //   ],
-  //   autoScroll: true,
-  //   // dragMoveListener from the dragging demo above
-  //   onmove: dragMoveListener
-  // })
-
-
+  }
 }
 
 </script>
